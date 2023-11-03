ab2e3c3706936ee7ccaa044f7ed17aa2ec953745
gmargoth <84695064+gmargoth@users.noreply.github.com>
2023-11-03 11:07:33 -0600

    Update bch_imae_download.py

--- bch_imae_download.py
+++ bch_imae_download.py
@@ -3,48 +3,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pandas as pd
+import os
 
-def login_SISEE(username, password, save_to):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_experimental_option("prefs", {"download.default_directory": save_to})
-    driver = webdriver.Chrome(options=options)
-    driver.get('https://sisee.bch.hn/IMAE/Login.aspx')
+class descargar_IMAE:
+    def __init__(self, save_to):
+        self.save_to = save_to
+        self.driver = None
+        self.global_only = None
 
-    username_field = driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$UserName')
-    password_field = driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$Password')
-    submit_button = driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$LoginButton')
+    def login(self, username, password):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_experimental_option("prefs", {"download.default_directory": self.save_to})
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get('https://sisee.bch.hn/IMAE/Login.aspx')
 
-    username_field.send_keys(username)
-    password_field.send_keys(password)
-    submit_button.click()
-    return driver 
+        username_field = self.driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$UserName')
+        password_field = self.driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$Password')
+        submit_button = self.driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$LoginButton')
 
-def click_button_and_wait(driver, button_locator, wait_time=10):
-    time.sleep(2)
-    btn = driver.find_element(By.NAME, button_locator[0])
-    btn.click()
-    wait = WebDriverWait(driver, wait_time)
-    btn_all = wait.until(EC.presence_of_element_located((By.NAME, button_locator[1])))
-    btn_all.click()
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        submit_button.click()
 
-def downaload_IMAE_query(driver, global_only):
-    time.sleep(1)
-    driver.get('https://sisee.bch.hn/IMAE/ReportePublicaciones.aspx')
+    def click_button_and_wait(self, button_locator, wait_time=10):
+        time.sleep(2)
+        btn = self.driver.find_element(By.NAME, button_locator[0])
+        btn.click()
+        wait = WebDriverWait(self.driver, wait_time)
+        btn_all = wait.until(EC.presence_of_element_located((By.NAME, button_locator[1])))
+        btn_all.click()
 
-    if global_only:
-        global_imae = driver.find_element(By.XPATH, "//input[@name='ctl00$ContentPlaceHolder1$RadioButtonList1'][@value='4']")
-        global_imae.click()
+    def descargar_IMAE_query(self, global_only):
+        time.sleep(1)
+        self.global_only = global_only
+        self.driver.get('https://sisee.bch.hn/IMAE/ReportePublicaciones.aspx')
+        button_locators = [('ctl00$ContentPlaceHolder1$BtnAños', 'ctl00$ContentPlaceHolder1$SeleccionardeListaAños$BtnSeleccionarTodo'),
+                           ('ctl00$ContentPlaceHolder1$BtnMeses', 'ctl00$ContentPlaceHolder1$SeleccionardeListaMes$BtnSeleccionarTodo'),]
+        imae_slct_all = ('ctl00$ContentPlaceHolder1$BtnIMAE', 'ctl00$ContentPlaceHolder1$SeleccionardeListaIMAE$BtnSeleccionarTodo')
 
-    button_locators = [('ctl00$ContentPlaceHolder1$BtnAños','ctl00$ContentPlaceHolder1$SeleccionardeListaAños$BtnSeleccionarTodo'),
-                        ('ctl00$ContentPlaceHolder1$BtnMeses', 'ctl00$ContentPlaceHolder1$SeleccionardeListaMes$BtnSeleccionarTodo'),
-                        ('ctl00$ContentPlaceHolder1$BtnIMAE', 'ctl00$ContentPlaceHolder1$SeleccionardeListaIMAE$BtnSeleccionarTodo'),
-                        ]
+        if global_only:
+            global_imae = self.driver.find_element(By.XPATH, "//input[@name='ctl00$ContentPlaceHolder1$RadioButtonList1'][@value='4']")
+            global_imae.click()
+        else:
+            button_locators.append(imae_slct_all)
 
-    for button_locator in button_locators:
-        click_button_and_wait(driver, button_locator)
-    
-    time.sleep(1)
-    download_btn = driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$BtnDescargar')
-    download_btn.click()
-    time.sleep(5)
+        for button_locator in button_locators:
+            self.click_button_and_wait(button_locator)
+
+        time.sleep(1)
+        download_btn = self.driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$BtnDescargar')
+        download_btn.click()
+        time.sleep(5)
+        self.driver.quit()
+
+    def view_download(self, delete_file):
+        if self.global_only:
+            f_type = "xls"
+        else:
+            f_type = 'xlsx'
+
+        file = os.path.join(self.save_to, 'IndiceMensualIMAE.' + f_type)
+        data = pd.read_excel(file)
+        file_path = file  # Replace with the path to the file you want to delete
+
+        if delete_file:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print(f"{file_path} eliminado.")
+            else:
+                print(f"{file_path} eliminado.")
+
+        return data
